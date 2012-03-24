440174289889c73d3f0610f52d9702220d4f9c71
Lachlan Priest <lachlanpriest@gmail.com>
2012-03-24 19:44:25 +1300

    add title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "Home page" do
+  end
+  
+  describe "Help page" do
+  end
+  
+  describe "About page" do
+  end
+  
+  describe "Contact page" do
+  end
+  
+end
