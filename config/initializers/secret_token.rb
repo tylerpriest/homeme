9ef119bca30b8e0dc5a6e71fa825b87deb557c42
Lachlan Priest <lachlanpriest@gmail.com>
2012-03-24 16:11:57 +1300

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeme::Application.config.secret_token = 'aecd84115a1dd04feeebb54d612e4b9b1035ae1408a868a5f36bdbda8c652efca49bbc26a5fafa57ea33e536217250c849f0a7cb806694b70b55353e6ae8380d'
