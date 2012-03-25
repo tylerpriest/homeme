7eb164cd0f7e4d579351385fc876951e8c471d82
Lachlan Priest <lachlanpriest@gmail.com>
2012-03-25 19:19:40 +1300

    Make basic User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,8 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('h1',    text: 'Reposit') }
     it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
@@ -30,6 +29,6 @@ describe "Static pages" do
     before { visit contact_path }
 
     it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it { should have_selector('title', text: full_title('Contact Us')) }
   end
 end
