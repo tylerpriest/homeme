4f1ccfc870bef445ffe17b71caeef85be43b6153
Lachlan Priest <lachlanpriest@gmail.com>
2012-03-27 12:54:27 +1300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,9 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+    
   it { should be_valid }
 
   describe "when name is not present" do
@@ -102,5 +104,10 @@ describe User do
                 specify { user_for_invalid_password.should be_false }
               end
             end
-          end
-        
+            
+            describe "remember token" do
+                before { @user.save }
+                its(:remember_token) { should_not be_blank }
+              end
+            end
+         
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,6 +35,37 @@ describe "User pages" do
             click_button "Create my account"
           end.to change(User, :count).by(1)
         end
+        
+        describe "after saving the user" do
+          before { click_button "Create my account" }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          it { should have_link('Sign out') }
+          
+        end
       end
     end
+  end
+    describe "signin" do
+
+        before { visit signin_path }
+
+        describe "with invalid information" do
+          before { click_button "Sign in" }
+
+          it { should have_selector('title', text: 'Sign in') }
+          it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+          describe "after visiting another page" do
+            before { click_link "Home" }
+            it { should_not have_selector('div.alert.alert-error') }
+          end
+          
+          describe "followed by signout" do
+                before { click_link "Sign out" }
+                it { should have_link('Sign in') }
+              end
+        end
   end
